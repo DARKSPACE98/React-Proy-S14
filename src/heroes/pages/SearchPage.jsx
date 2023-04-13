e0cab7232af21fd020e963012852edf1764e824c
DARKSPACE98 <enriquechuyin2019@gmail.com>
2023-04-12 23:49:40 -0700

    Se agregaron modificaciones al search para que funcione mejor y se crearon archivos nuevos para la autentificacion del Login

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -11,8 +11,10 @@ export const SearchPage = () => {
   const location = useLocation();
 
   const { q = '' } = queryString.parse( location.search )
-  
   const heroes = getHeroesByName(q);
+
+  const showSearch =( q.length === 0 );
+  const showError = ( q.length >0 )&& heroes.length === 0;
 
   const { searchText, onInputChange } = useForm({
     searchText: ""
@@ -20,7 +22,7 @@ export const SearchPage = () => {
 
   const onSearchSubmit = (event) => {
     event.preventDefault();
-    if( searchText.trim().length <= 1)return;
+   // if( searchText.trim().length <= 1)return;
     
     navigate(`?q=${ searchText }`);
 
@@ -57,12 +59,21 @@ export const SearchPage = () => {
         <h4>Results</h4>
         <hr />
 
-        <div className="alert alert-primary">
-          Search a hero
-        </div>
+        {/* {
+          ( q === '' )
+          ? <div className="alert alert-primary">Search a hero</div>
+          :( heros.length === 0 )
+          &&<div className="alert alert-danger">
+          No hero with <b>{ q }</b>
+         </div>
 
-        <div className="alert alert-danger">
-         No hero with <b>{ q }</b>
+        }     */}   
+
+        <div className="alert alert-primary animate__animated animate__fadeInDown" style={{ display: showSearch ? '' : 'none' }}>
+          Search a hero</div> 
+
+        <div className="alert alert-danger animate__animated animate__fadeInDown" style={{ display: showError ? '': 'none' }}>
+          No hero with <b>{ q }</b>
         </div>
      
       {
